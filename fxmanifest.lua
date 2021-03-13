cfec67ddaa35a1bcafaad51dab8b9466b38c4447
Abdulrahman Abdallah <68659094+alMajor@users.noreply.github.com>
2021-03-13 15:23:36 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+
+game  'gta5'
+
+author 'alMajor'
+description 'a script for removing creating peds'
+
+client_scripts {
+    'main.lua',
+}
---
+++ main.lua
@@ -0,0 +1,5 @@
+AddEventHandler('populationPedCreating', function(x, y, z, model, setters)
+    ClearAreaOfPeds(201.97, -938.78, 30.69, 0x10000000000.0, 1)
+    CancelEvent()
+end)
+
